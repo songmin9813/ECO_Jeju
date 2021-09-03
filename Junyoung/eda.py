15a5fc17606952ceda115aff8b0dc9e50694bfc0
Lee-jun-young98 <80506107+Lee-jun-young98@users.noreply.github.com>
2021-09-03 16:25:51 +0900

    2021-09-03 주석

--- Junyoung/eda.py
+++ Junyoung/eda.py
@@ -7,21 +7,20 @@ Created on Mon Aug 30 22:20:00 2021
 
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
-from xgboost import XGBRegressor
-from catboost import CatBoostRegressor
-import lightgbm as LGB
-from sklearn.cross_decomposition import PLSRegression
-from sklearn.linear_model import Lasso,ElasticNet,Ridge
-from sklearn.svm import SVR
-from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import StandardScaler
-from sklearn.impute import SimpleImputer
-import statsmodels.api as sm
+from sklearn.preprocessing import LabelEncoder ## 라벨인코딩을 위해 사용
+from sklearn.preprocessing import StandardScaler ## StandardScaler를 위해 사용
+from sklearn.model_selection import train_test_split ## train_test셋을 나누기 위해 사용
+from sklearn.ensemble import RandomForestRegressor ## 랜덤포레스트 회귀모델
+from sklearn.linear_model import LinearRegression ## 선형 회귀모델
+from xgboost import XGBRegressor ## XGBoost 회귀모델
+from catboost import CatBoostRegressor ## CatBoost 회귀모델
+import lightgbm as LGB ## LGBM 모델
+from sklearn.cross_decomposition import PLSRegression ## PLS 회귀모델
+from sklearn.linear_model import Lasso,ElasticNet,Ridge ## Lasso, Ridge, ElasticNet 회귀모델
+from sklearn.svm import SVR ## SVM 회귀모델
+from sklearn.metrics import mean_squared_error ## 평가지표로 사용할 MSE
+from sklearn.impute import SimpleImputer ## 결측값 처리 패키지
+import statsmodels.api as sm ## 단순선형회귀분석에 사용
 
 
 # path = "D:/git_project/ECO_Jeju/Sungmin/new_datas/2nd_edition"
@@ -42,6 +41,8 @@ res = pd.read_csv(path + "/resident_group.csv")
 card = pd.read_csv(path + "/card_group.csv")
 
 
+## 라벨인코더는 범주형 변수를 명목형 변수(수치형)으로 변환 할때 사용
+## 피처들의 단위가 모두 다르므로 StandardScaler를 사용 -> 이상치가 있나 없나 탐지해봐야함
 le = LabelEncoder()
 sc = StandardScaler()
 #%% waste 전처리
@@ -59,6 +60,8 @@ dict_={}
 for i,value in enumerate(new_list):
     dict_[value]=i
 
+
+## le.fit을 이용해 명목형변수로 변경
 le.fit(waste["emd_nm"])
 emd_nm = le.transform(waste["emd_nm"])
 new_waste["emd_nm"] = emd_nm
@@ -110,7 +113,6 @@ new_long = long
 
 new_long.columns
 
-
 new_long = new_long.drop(['long_nationality_AUS', 'long_nationality_BGD',
        'long_nationality_CAN', 'long_nationality_CHN', 'long_nationality_DEU',
        'long_nationality_EGY', 'long_nationality_ETC', 'long_nationality_FRA',
@@ -155,7 +157,8 @@ new_short["emd_nm"] = emd_nm
 
 new_short = new_short.set_index(["base_date", "emd_nm"])
 
-#%% resdient 전처리 년도와 월을합쳐서 진행해보자!!!
+#%% resdient 전처리 년도와 월을합쳐서 하나의 피처로 만들어서 진행해야 할 꺼같다
+# -> resdient 데이터를 사용할꺼면 다른 데이터들도 월단위로 바꿔야할 꺼 같다
 
 res
 res.info()
@@ -190,27 +193,36 @@ data = pd.concat([new_waste, new_korean, new_long, new_short, new_card], axis = 
 col = data.columns
 data_index = data.index
 
+
+## 결측값을 각 피처들의 평균값으로 대체하기 위해 SimpleImputer 사용
 imputer = SimpleImputer(strategy = 'mean')
 imp_data = pd.DataFrame(imputer.fit_transform(data))
 imp_data.columns = col
 imp_data = imp_data.set_index(data_index)
 
-x = imp_data.drop("waste_em_g", axis=1)
-y = np.array(imp_data["waste_em_g"])
-x.columns
 
+
+## 이 부분에서 변수를 삭제해서 더 진행할 수 있다.
+x = imp_data.drop(["waste_em_g", "korean_resd_pop_cnt"], axis=1)
+x.columns
+y = np.array(imp_data["waste_em_g"])
+
+# StandardScaler를 통해 값을 정규화 시킨다
 sc_x = sc.fit_transform(x)
 sc_y = sc.fit_transform(y.reshape(-1,1))
 
 
+# 모델링을 하기 위해 train과 test셋의 비율을 7:3으로 나눈다. 
 train_x, test_x, train_y, test_y = train_test_split(sc_x, sc_y, test_size = 0.3, random_state = 100)
 
-#%% 단순선형회귀분석
+#%% 단순선형회귀분석  
 
-
-model1 = sm.OLS(train_y, train_x)
+model1 = sm.OLS(train_y, train_x) 
 fitted_model = model1.fit()
-fitted_model.summary()
+fitted_model.summary() 
+
+# 1.korean_resd_pop_cnt 변수의 p-value 값이 0.05보다 높아서 삭제
+## 전부 0.05안에 들어 유의미한 변수이며 R결정계수값이 0.979로 높으편이다
 
 
 #%% 모델링
